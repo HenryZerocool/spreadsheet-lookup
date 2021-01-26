7a2b897ac388ad228e96d0d180633e175f0a6975
Phan Hieu Nguyen <fanhieu94@gmail.com>
2021-01-25 22:32:38 -0500

    update input handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Container } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import "./App.css";
 
@@ -31,12 +31,19 @@ function App() {
   const [storage, setStorage] = useState('');
   const [buyinPrice, setBuyinPrice] = useState('');
 
-  function handleLookup(lookupValue) {
-    const indexOfLookup = googleData.values[0].indexOf(lookupValue);
-    if (indexOfLookup >= 0) {
-      setMeasurement(googleData.values[1][indexOfLookup]);
-      setStorage(googleData.values[2][indexOfLookup]);
-      setBuyinPrice(googleData.values[3][indexOfLookup]);
+  function handleLookup(newLookupValue) {
+    // handle null or empty input in console error
+    if (!newLookupValue)
+      newLookupValue = '';
+    // only update look up if new input is entered
+    if (newLookupValue.toString().trim().length > 0 && lookupValue !== newLookupValue){
+      setLookupValue(newLookupValue);
+      const indexOfLookup = googleData.values[0].indexOf(newLookupValue);
+      if (indexOfLookup >= 0) {
+        setMeasurement(googleData.values[1][indexOfLookup]);
+        setStorage(googleData.values[2][indexOfLookup]);
+        setBuyinPrice(googleData.values[3][indexOfLookup]);
+      }
     }
   }
 
@@ -63,56 +70,57 @@ function App() {
         }
         {
           //#region loginform
-        /* <form onSubmit={handleLogin}>
-          <TextField
-            // required
-            id="user-required"
-            label="Future User"
-            variant="outlined"
-            name="username"
-            value={username}
-            onInput={e => setUsername(e.target.value)}
-          />
-          <TextField
-            id="password-login"
-            label="Password"
-            type="password"
-            // autoComplete="current-password"
-            variant="outlined"
-            name="password"
-            value={password}
-            onInput={e => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-          >
-            Log In
-          </Button>
-        </form> */ 
-        //#endregion
+          /* <form onSubmit={handleLogin}>
+            <TextField
+              // required
+              id="user-required"
+              label="Future User"
+              variant="outlined"
+              name="username"
+              value={username}
+              onInput={e => setUsername(e.target.value)}
+            />
+            <TextField
+              id="password-login"
+              label="Password"
+              type="password"
+              // autoComplete="current-password"
+              variant="outlined"
+              name="password"
+              value={password}
+              onInput={e => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              Log In
+            </Button>
+          </form> */
+          //#endregion
         }
-        <div>
-          <Autocomplete
-            id="lookup-input"
-            value={lookupValue}
-            onChange={(event, newLookupValue) => {
-              setLookupValue(newLookupValue);
-            }}
-            onInputChange={(event, newLookupValue) => {
-              handleLookup(newLookupValue);
-            }}
-            options={googleData.values[0]}
-            style={{ width: 300 }}
-            renderInput={(params) => (
-              <TextField {...params} label="Input" variant="outlined" />
-            )}
-          /></div>
-        <div>{`value: ${lookupValue !== null ? `'${lookupValue}'` : 'null'}`}</div>
-        <div>{`measurement : '${measurement}'`}</div>
-        <div>{`price per '${measurement}' : ${buyinPrice} 'VND'`}</div>
-        <div>{`storage : ${storage} '${measurement}'`}</div>
+        <Container maxWidth="sm">
+          <div>
+            <Autocomplete
+              id="lookup-input"
+              value={lookupValue}
+              onChange={(event, newLookupValue) => {
+                handleLookup(newLookupValue);
+              }}
+              options={googleData.values[0]}
+              style={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label="Input" variant="outlined" />
+              )}
+            /></div>
+          {/* <div>{`value: ${lookupValue !== null ? `'${lookupValue}'` : 'null'}`}</div> */}
+          <TextField id="name-output" label="name" value={lookupValue} variant="outlined" disabled />
+          <TextField id="measurement-output" label="measurement" value={measurement} variant="outlined" disabled />
+          <TextField id="buyin-price-output" label="buyin-price" value={(buyinPrice * 1).toFixed(2)} variant="outlined" disabled />
+          <TextField id="sellling-price-output" label="sellling-price" value={(buyinPrice * 1.15).toFixed(2)} variant="outlined" disabled />
+          <TextField id="storage-output" label="storage" value={storage} variant="outlined" disabled />
+        </Container>
       </div>
     );
   } else {
